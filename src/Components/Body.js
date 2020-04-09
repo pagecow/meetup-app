89cdde755fb1e178d2bd763c7da28c97038cfd81
pagecow <pagecow@gmail.com>
2020-04-09 16:58:40 -0600

    more styling finished

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import { Button, Container, Typography, makeStyles } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 
 const useStyles = makeStyles((theme) => ({
     secondaryLight: {
         color: theme.palette.secondary.light,
+        fontSize: "14px",
     },
     secondaryDark: {
         color: theme.palette.secondary.dark,
+        fontSize: "14px",
     },
     boldText: {
         fontWeight: 900,
@@ -20,13 +25,28 @@ const useStyles = makeStyles((theme) => ({
     },
     whiteCont: {
         display: "flex",
-        height: "96px",
         width: "300px",
         backgroundColor: "white",
         padding: "18px",
         outline: "0px",
         borderRadius: "8px",
         margin: "40px 0 0 0",
+    },
+    whiteCont2: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "300px",
+        width: "300px",
+        backgroundColor: "white",
+        padding: "18px",
+        outline: "0px",
+        borderRadius: "8px",
+        margin: "40px 0 0 0",
+    },
+    icons: {
+        marginRight: "20px",
+        color: theme.palette.secondary.light,
     }
 }))
 
@@ -62,7 +82,11 @@ function Body(props){
     };
 
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let options2 = { timeZone: 'America/Chicago', hour: '2-digit', minute: '2-digit' };
     let date  = new Date(event_info.time);
+    let endTime  = new Date(event_info.time + event_info.duration);
+    console.log(date);
+    console.log(endTime);
 
     function createMarkup() {
         return {__html: event_info.description};
@@ -76,13 +100,44 @@ function Body(props){
                     <Typography className={classes.breakText} dangerouslySetInnerHTML={createMarkup()} />
                 </Container>
 
-                <Container className={classes.whiteCont}>
-                    <img className="group-img" src={thumbnail} />
-                    <div>
-                        <Typography className={classes.boldText}>{name}</Typography>
-                        <Typography className={classes.secondaryLight}>{visibility} group</Typography>
-                    </div>
-                </Container>
+                <div>
+                    <Container className={classes.whiteCont}>
+                        <img className="group-img" src={thumbnail} />
+                        <div>
+                            <Typography className={classes.boldText}>{name}</Typography>
+                            <Typography className={classes.secondaryLight}>{visibility} group</Typography>
+                        </div>
+                    </Container>
+
+                    <Container className={classes.whiteCont2}>
+                        <div className="event-info">
+                            <CheckIcon className={classes.icons} color="secondary" />
+                            <div>
+                                <Typography className={classes.secondaryDark}>RSVP Opens</Typography>
+                                <Typography className={classes.secondaryDark}>{date.toLocaleDateString("en-US", options)}</Typography>
+                                <Typography className={classes.secondaryDark}>{date.toLocaleTimeString("en-US", options2)}</Typography>
+                            </div>
+                        </div>
+                        <div className="event-info">
+                            <ScheduleIcon className={classes.icons} color="secondary" />
+                            <div>
+                                <Typography className={classes.secondaryDark}>{date.toLocaleDateString("en-US", options)}</Typography>
+                                <Typography className={classes.secondaryDark}>{date.toLocaleTimeString("en-US", options2)} to {endTime.toLocaleTimeString("en-US", options2)}</Typography>
+                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
+                                <Typography className={classes.secondaryDark}>Add to calendar (Not Finished)</Typography>
+                            </div>
+                        </div>
+                        <div className="event-info">
+                            <LocationOnOutlinedIcon className={classes.icons} color="secondary" />
+                            <div>
+                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
+                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
+                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
+                            </div>
+                        </div>
+                        
+                    </Container>
+                </div>
             </Container>
         </div>
     )
