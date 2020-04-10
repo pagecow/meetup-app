088e3d307c46194a70d91dc8f178a5df3a8fb4e8
pagecow <pagecow@gmail.com>
2020-04-10 10:08:51 -0600

    map feature working

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
-import { Button, Container, Typography, makeStyles } from '@material-ui/core';
+import { Container, Typography, makeStyles } from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const useStyles = makeStyles((theme) => ({
     secondaryLight: {
@@ -12,6 +16,10 @@ const useStyles = makeStyles((theme) => ({
     },
     secondaryDark: {
         color: theme.palette.secondary.dark,
+        fontSize: "14px",
+    },
+    link: {
+        color: "#00a2c7",
         fontSize: "14px",
     },
     boldText: {
@@ -25,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
     },
     whiteCont: {
         display: "flex",
-        width: "300px",
+        width: "330px",
         backgroundColor: "white",
         padding: "18px",
         outline: "0px",
@@ -36,13 +44,13 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
-        height: "300px",
-        width: "300px",
+        height: "500px",
+        width: "330px",
         backgroundColor: "white",
         padding: "18px",
         outline: "0px",
         borderRadius: "8px",
-        margin: "40px 0 0 0",
+        margin: "40px 0 200px 0",
     },
     icons: {
         marginRight: "20px",
@@ -68,14 +76,26 @@ function Body(props){
     if(!rsvps_info.member){
         name = null;
     } else {
-        thumbnail = rsvps_info.group.group_photo.thumb_link
+        thumbnail = rsvps_info.group.group_photo.thumb_link;
     };
 
-    let visibility;
-    if(!event_info.visibility){
+    let visibility, latitude, longitude, location, address, city, state;
+    if(!event_info){
         visibility = null;
-    } else if (event_info.visibility) {
+        latitude = null;
+        longitude = null;
+        location = null;
+        address = null;
+        city = null;
+        state = null;
+    } else if (event_info.visibility && event_info.venue.lat && event_info.venue.lon && event_info.venue.name) {
         visibility = event_info.visibility;
+        latitude = event_info.venue.lat;
+        longitude = event_info.venue.lon;
+        location = event_info.venue.name;
+        address = event_info.venue.address_1;
+        city = event_info.venue.city;
+        state = event_info.venue.state;
     }; 
     if (visibility) {
         visibility = event_info.visibility.charAt(0).toUpperCase() + visibility.slice(1);
@@ -85,8 +105,6 @@ function Body(props){
     let options2 = { timeZone: 'America/Chicago', hour: '2-digit', minute: '2-digit' };
     let date  = new Date(event_info.time);
     let endTime  = new Date(event_info.time + event_info.duration);
-    console.log(date);
-    console.log(endTime);
 
     function createMarkup() {
         return {__html: event_info.description};
@@ -100,7 +118,7 @@ function Body(props){
                     <Typography className={classes.breakText} dangerouslySetInnerHTML={createMarkup()} />
                 </Container>
 
-                <div>
+                <div className="right-info">
                     <Container className={classes.whiteCont}>
                         <img className="group-img" src={thumbnail} />
                         <div>
@@ -114,7 +132,7 @@ function Body(props){
                             <CheckIcon className={classes.icons} color="secondary" />
                             <div>
                                 <Typography className={classes.secondaryDark}>RSVP Opens</Typography>
-                                <Typography className={classes.secondaryDark}>{date.toLocaleDateString("en-US", options)}</Typography>
+                                <Typography className={classes.secondaryDark}>Tuesday, May 12, 2020</Typography>
                                 <Typography className={classes.secondaryDark}>{date.toLocaleTimeString("en-US", options2)}</Typography>
                             </div>
                         </div>
@@ -123,19 +141,34 @@ function Body(props){
                             <div>
                                 <Typography className={classes.secondaryDark}>{date.toLocaleDateString("en-US", options)}</Typography>
                                 <Typography className={classes.secondaryDark}>{date.toLocaleTimeString("en-US", options2)} to {endTime.toLocaleTimeString("en-US", options2)}</Typography>
-                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
-                                <Typography className={classes.secondaryDark}>Add to calendar (Not Finished)</Typography>
+                                <Typography className={classes.secondaryDark}>Every 2nd Tuesday of the month</Typography>
+                                <a className={classes.link} href="https://calendar.google.com" target="_blank">Add to calendar</a>
                             </div>
                         </div>
                         <div className="event-info">
                             <LocationOnOutlinedIcon className={classes.icons} color="secondary" />
                             <div>
-                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
-                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
-                                <Typography className={classes.secondaryDark}>(Not Finished)</Typography>
+                                <a href={`https://www.google.com/maps/search/?api=1&query=${latitude}%2C${longitude}`} target="_blank" className={classes.secondaryDark}>{location}</a>
+                                <Typography className={classes.secondaryLight}>{address} * {city}, {state}</Typography>
+                                <Typography className={classes.secondaryDark} style={{paddingTop: "5px"}}>How to find us</Typography>
                             </div>
                         </div>
-                        
+                        <div style={{height: "200px", width: "330px", margin: "-18px"}}>
+                            <GoogleMapReact 
+                                bootstrapURLKeys={{ key: 'AIzaSyAsvChBYmLNEGzqxdiHOzXQRuKv1KCgTGY', language: 'en' }}
+                                center={{lat: latitude, lng: longitude}}
+                                defaultZoom={12}
+                                >
+                                <a href={`https://www.google.com/maps/search/?api=1&query=${latitude}%2C${longitude}`} target="_blank">
+                                    <LocationOnIcon
+                                        lat={latitude}
+                                        lng={longitude}
+                                        fontSize="large"
+                                        color="primary"
+                                        />
+                                </a>
+                            </GoogleMapReact>
+                        </div>    
                     </Container>
                 </div>
             </Container>
